82b6f2d41aac31af8fa56e49cb96202926fc94f1
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 09:53:34 +0300

    Update resume.ts

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -1,11 +1,11 @@
 // src/commands/music/resume.ts
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { lavalinkService } from '../../bot';
 
 export default {
   data: new SlashCommandBuilder().setName('resume').setDescription('Возобновить воспроизведение'),
   async execute(interaction: ChatInputCommandInteraction) {
-    const clientAny = interaction.client as any;
-    const player = clientAny.lavalink.manager.players.get(interaction.guildId!);
+	const player = lavalinkService.lavashark.players.get(interaction.guildId!);
     if (!player || !player.paused) {
       return interaction.reply({ content: 'Нечего возобновлять.', ephemeral: true });
     }
