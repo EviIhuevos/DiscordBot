29ffbf41c5a2038ecae24cfa5081e38a8c7c3a40
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 10:02:17 +0300

    Update queue.ts

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -5,11 +5,11 @@ import { lavalinkService } from '../../bot';
 export default {
   data: new SlashCommandBuilder().setName('queue').setDescription('Показать очередь треков'),
   async execute(interaction: ChatInputCommandInteraction) {
-	const player = lavalinkService.lavashark.players.get(interaction.guildId!);
-    if (!player || !player.queue.length) {
+	const player: any = lavalinkService.lavashark.players.get(interaction.guildId!);
+    if (!player || !player.queue?.length) {
       return interaction.reply({ content: 'Очередь пуста.', ephemeral: true });
     }
-    const list = player.queue
+    const list = (player.queue as any[])
       .map((track: any, i: any) => `${i + 1}. ${track.title}`)
       .slice(0, 10)
       .join('\n');
