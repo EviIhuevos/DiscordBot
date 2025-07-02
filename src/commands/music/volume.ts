173745af6433e28e755bacbcf5e1e6158fdc7f57
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 09:52:40 +0300

    Update volume.ts

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -1,5 +1,6 @@
 // src/commands/music/volume.ts
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { lavalinkService } from '../../bot';
 
 export default {
   data: new SlashCommandBuilder()
@@ -7,12 +8,11 @@ export default {
     .setDescription('Установить громкость (0-100)')
     .addIntegerOption(opt => opt.setName('level').setDescription('Уровень громкости').setRequired(true)),
   async execute(interaction: ChatInputCommandInteraction) {
-    const clientAny = interaction.client as any;
-    const vol = interaction.options.getInteger('level', true);
+	const vol = interaction.options.getInteger('level', true);
     if (vol < 0 || vol > 100) {
       return interaction.reply({ content: 'Громкость должна быть от 0 до 100.', ephemeral: true });
     }
-    const player = clientAny.lavalink.manager.players.get(interaction.guildId!);
+    const player = lavalinkService.lavashark.players.get(interaction.guildId!);
     if (!player) {
       return interaction.reply({ content: 'Плеер не найден.', ephemeral: true });
     }
