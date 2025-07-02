8ea39a11549304eb6c69cdedb5f7ed71d02a02ac
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 10:01:59 +0300

    Update skip.ts

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -5,12 +5,12 @@ import { lavalinkService } from '../../bot';
 export default {
   data: new SlashCommandBuilder().setName('skip').setDescription('Пропустить текущий трек'),
   async execute(interaction: ChatInputCommandInteraction) {
-	const player = lavalinkService.lavashark.players.get(interaction.guildId!);
-    if (!player || !player.queue.current) {
+	const player: any = lavalinkService.lavashark.players.get(interaction.guildId!);
+    if (!player || !player.queue?.current) {
       return interaction.reply({ content: 'Нет трека для пропуска.', ephemeral: true });
     }
     const current = player.queue.current;
-    player.stop();
+    (player as any).stop();
     await interaction.reply(`Пропущен: **${current.title}**`);
   },
 };
