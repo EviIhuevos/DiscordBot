1dc328f63b696dd2cd21357bc3dd6c9a4b1f2087
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 09:52:51 +0300

    Update stop.ts

--- src/commands/music/stop.ts
+++ src/commands/music/stop.ts
@@ -1,11 +1,11 @@
 // src/commands/music/stop.ts
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { lavalinkService } from '../../bot';
 
 export default {
   data: new SlashCommandBuilder().setName('stop').setDescription('Остановить воспроизведение и очистить очередь'),
   async execute(interaction: ChatInputCommandInteraction) {
-    const clientAny = interaction.client as any;
-    const player = clientAny.lavalink.manager.players.get(interaction.guildId!);
+	const player = lavalinkService.lavashark.players.get(interaction.guildId!);
     if (!player) {
       return interaction.reply({ content: 'Плеер не найден.', ephemeral: true });
     }
