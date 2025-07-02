3fe72add104d77a629ab0a09642940d21e2aaaa9
EviIhuevos <159585011+EviIhuevos@users.noreply.github.com>
2025-07-02 09:53:59 +0300

    Update pause.ts

--- src/commands/music/pause.ts
+++ src/commands/music/pause.ts
@@ -1,11 +1,11 @@
 // src/commands/music/pause.ts
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { lavalinkService } from '../../bot';
 
 export default {
   data: new SlashCommandBuilder().setName('pause').setDescription('Поставить паузу'),
   async execute(interaction: ChatInputCommandInteraction) {
-    const clientAny = interaction.client as any;
-    const player = clientAny.lavalink.manager.players.get(interaction.guildId!);
+    const player = lavalinkService.lavashark.players.get(interaction.guildId!);
     if (!player || !player.playing) {
       return interaction.reply({ content: 'Нечего паузить.', ephemeral: true });
     }
